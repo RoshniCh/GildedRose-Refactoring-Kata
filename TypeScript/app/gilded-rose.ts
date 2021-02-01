f4fcd2ed95d7240d38df59a9074a8186cbbfb67b
RoshniCh <roshni.chandrasekar@softwire.com>
2021-02-01 18:04:07 +0000

    all categories including conjure separated,string search, if tidy up

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -42,23 +42,17 @@ export class GildedRose {
 
     backstageUpdate(item: Item) : Item
     {
-        if (item.quality < 50)
-        {
+        if (item.quality < 50) {
             item.quality = item.quality + 1
         }
-        if (item.sellIn < 11) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1
-            }
+        if (item.sellIn < 11 && item.quality < 50) {
+           item.quality = item.quality + 1
         }
-        if (item.sellIn < 6) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1
-            }
+        if (item.sellIn < 6 && item.quality < 50) {
+                  item.quality = item.quality + 1
         }
         item.sellIn = item.sellIn - 1;
-        if (item.sellIn < 0)
-        {
+        if (item.sellIn < 0) {
             item.quality = item.quality - item.quality
         }
         return item;
@@ -71,10 +65,8 @@ export class GildedRose {
             item.quality = item.quality + 1
         }
         item.sellIn = item.sellIn - 1;
-        if (item.sellIn < 0) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1
-            }
+        if (item.sellIn < 0 && item.quality < 50) {
+            item.quality = item.quality + 1
         }
         return item;
     }
@@ -87,43 +79,33 @@ export class GildedRose {
 
     otherCatUpdate(item: Item) : Item
     {
-        if (item.quality > 0)
-        {
+        if (item.quality > 0) {
             item.quality = item.quality - 1
         }
         item.sellIn = item.sellIn - 1;
-        if (item.sellIn < 0) {
-            if (item.quality > 0)
-            {
+        if (item.sellIn < 0 && item.quality > 0) {
             item.quality = item.quality - 1
-            }
         }
         return item;
     }
 
     conjuredUpdate(item: Item) : Item
     {
-        if (item.quality > 0)
-        {
+        if (item.quality > 0) {
             item.quality = item.quality - 1
         }
-        if (item.quality > 0)
-        {
+        if (item.quality > 0) {
             item.quality = item.quality - 1
         }
         item.sellIn = item.sellIn - 1;
         if (item.sellIn < 0) {
-            if (item.quality > 0)
-            {
+            if (item.quality > 0) {
             item.quality = item.quality - 1
             }
-            if (item.quality > 0)
-            {
+            if (item.quality > 0) {
             item.quality = item.quality - 1
             }
         }
         return item;
     }
-    
-
 }
